4f73e2b09c59ae8fe15cd5a02fe0cd09c45697ae
william george <williameverettgeorge@icloud.com>
2018-09-29 17:43:53 +0000

    automatically backed up by learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,3 +1,6 @@
 def greeting
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
 end
+puts "Enter your name helper_method"
+name = gets.chomp()
+greeting("Partrice and Blair")
